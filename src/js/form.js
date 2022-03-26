fadb5116315811a4c36869d02ce2dc613f44e8d5
DmitriiPro <cf.gwod@gmail.com>
2022-03-27 01:02:29 +0300

    адаптировал блок help, но не подключил swiper

--- src/js/form.js
+++ src/js/form.js
@@ -38,3 +38,39 @@ const form = () => {
 };
 
 form();
+
+const swiper = () => {
+//     const swiper = new Swiper('.swiper1', {
+//         // Optional parameters
+//         direction: 'vertical',
+//         loop: true,
+      
+//         // If we need pagination
+//         pagination: {
+//           el: '.swiper-pagination',
+//         },
+      
+//         // Navigation arrows
+//         navigation: {
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//         },
+      
+//         // And if we need scrollbar
+//         scrollbar: {
+//           el: '.swiper-scrollbar',
+//         },
+//       });
+//     const sliderAll = () => {
+//         const swiper1 = new Swiper('.swiper1', {
+//             slidesPerView: 1,
+//         });  
+        
+//         const swiper2 = new Swiper('.swiper2', {
+//             slidesPerView: 1,
+//         });  
+//     };
+    
+
+}
+swiper();
